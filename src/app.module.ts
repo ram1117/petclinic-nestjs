f55df2989e9cb0a356316ee4b841b767bea5cf9a
ram1117 <ramkumark1@outlook.com>
2024-02-26 20:09:49 +0530

    add controller and routes for report

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PetsModule } from './pets/pets.module';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, PetsModule, AppointmentsModule],
+  imports: [UsersModule, PetsModule, AppointmentsModule, ReportsModule],
   controllers: [],
   providers: [],
 })
